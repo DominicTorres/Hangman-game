092bb57657531ab0085692f2f1506f371597294e
Dominic Torres <dtorres243@west-mec.org>
2021-03-05 17:02:26 -0700

    from last friday

---
+++ hangman.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+    var alphabet = ['a','b','c','d','e','f','g','h','i',
+    'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    var categories;
+    var guess;
+    var storedGuesses;
+    var word;
+    var lives;
+    var space;
+    var counter;
+    var chosenCategories;
+    
+    
+
+}
